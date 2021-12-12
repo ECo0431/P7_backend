f5e2e9dcb1329ccc351ec6e4bf1be5f4424e7f85
ChatCode <chatcode@MacBook-Pro-de-ChatCode.local>
2021-12-12 21:30:47 +0100

    un commit en plus

--- routes/user.js
+++ routes/user.js
@@ -4,10 +4,10 @@ const userCtrl = require("../controllers/user");
 const auth = require("../middlewares/auth");
 
 router.get("/users", userCtrl.selectAllUsers);
-router.get("/users/:id_users", auth, userCtrl.selectOneUser);
+router.get("/users/:id_users", userCtrl.selectOneUser);
 router.post("/users/login", userCtrl.loginUser);
 router.post("/users/register", userCtrl.registerUsers);
-router.put("/users/:id_users", auth, userCtrl.updateUsers);
-router.delete("/users/:id_users", auth, userCtrl.deleteUsers);
+router.put("/users/:id_users", userCtrl.updateUsers);
+router.delete("/users/:id_users", userCtrl.deleteUsers);
 
 module.exports = router;
