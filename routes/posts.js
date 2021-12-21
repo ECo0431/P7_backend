a53a09c427be32ed76b35fb6f414b2b9ae02a99a
ChatCode <chatcode@MacBook-Pro-de-ChatCode.local>
2021-12-21 16:35:43 +0100

    Ajout post commentaire et post

--- routes/posts.js
+++ routes/posts.js
@@ -7,5 +7,6 @@ router.get("/posts", postsCtrl.selectAllPosts);
 router.get("/posts/:id_posts", postsCtrl.selectOnePost);
 router.post("/users/:id_users/posts", postsCtrl.insertOnePost);
 router.put("/users/:id_users/posts/:id_posts", postsCtrl.modifyOnePost);
+router.delete("/users/:id_users/posts/:id_posts", postsCtrl.deleteOnePost);
 
 module.exports = router;
